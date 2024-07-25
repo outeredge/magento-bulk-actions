85434fb98fcd18768df92273ace7dec9a1f9a1da
Bruno Dias <ibroopi@gmail.com>
2024-07-25 12:39:21 +0100

    Change order status

---
+++ Controller/Adminhtml/Order/MassOrderStatus.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OuterEdge\BulkActions\Controller\Adminhtml\Order;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Psr\Log\LoggerInterface;
+
+class MassOrderStatus extends \Magento\Backend\App\Action
+{
+    public function __construct(
+        Context $context,
+        protected ManagerInterface $messageManager,
+        protected OrderRepositoryInterface $orderRepository,
+        protected LoggerInterface $logger
+    ) {
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultRedirect = $this->resultRedirectFactory->create();
+
+        if (!empty($ordersId)) {
+            foreach ($ordersId as $orderId) {
+
+                $order = $this->orderRepository->get($orderId);
+                $order->setState(\Magento\Sales\Model\Order::STATE_COMPLETE);
+                $order->setStatus(\Magento\Sales\Model\Order::STATE_COMPLETE);
+
+                try {
+                    $this->orderRepository->save($order);
+                } catch (\Exception $e) {
+                    $this->logger->error($e);
+                    $this->messageManager->addExceptionMessage($e, $e->getMessage());
+                }
+            }
+        }
+        return $resultRedirect->setPath('sales/order/index', [], ['error' => true]);
+    }
+}
